bb4fd6671fed6396933c261e52a00d5e662fa830
goVishal <golakiyavishal812@gmail.com>
2022-08-11 12:57:58 +0530

    - initial commit

---
+++ app/src/main/java/com/go/androidplay/model/Post.kt
@@ -0,0 +1,20 @@
+package com.go.androidplay.model
+
+class Post {
+}
+
+
+
+
+/*
+import com.google.gson.annotations.SerializedName
+
+
+data class Post (
+
+    @SerializedName("userId" ) var userId : Int?    = null,
+    @SerializedName("id"     ) var id     : Int?    = null,
+    @SerializedName("title"  ) var title  : String? = null,
+    @SerializedName("body"   ) var body   : String? = null
+
+)*/
